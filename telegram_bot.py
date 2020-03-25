8b8e805b0e54257e489a5640cec444adb7b6601a
artkli <artur.klimek@gmail.com>
2020-03-25 16:57:35 +0100

    add - get active wifi users from unifi mongodb

--- telegram_bot.py
+++ telegram_bot.py
@@ -77,7 +77,7 @@ def wifi_users():
     for e in last_events:
         print(e)
         if e and e[1] == 'EVT_WU_Connected':
-            c = '(5GHz)' if e[4] == 36 else '(2.4GHz)'
+            c = '(5GHz)' if e[4] == '36' else '(2.4GHz)'
             r = r + str(i) + '. <b><code>' + e[5] + '</code></b> - ' + e[3] + c + '\n'
             i = i + 1
 
