378ede5fb66dc89c3c26df0604a1a23f76c53bd2
artkli <artur.klimek@gmail.com>
2020-05-23 16:58:57 +0200

    add logging

--- telegram_bot.py
+++ telegram_bot.py
@@ -44,7 +44,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler(LOGFILENAME)
 formatter    = logging.Formatter('%(asctime)s : %(message)s')
-#formatter    = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
